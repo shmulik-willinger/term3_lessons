d6884b89a76cbec55bd2740016fe68dba31e406f
Shmulik Willinger <shmulik83@gmail.com>
2018-01-03 23:26:48 +0200

    updating with Quiz 2.11

--- first_search_program.py
+++ first_search_program.py
@@ -44,6 +44,7 @@ def is_possible(new_place):
         return 0
     return 1
 
+
 def calc_cost(round):
     next_round = []
     for start in round:
@@ -52,19 +53,35 @@ def calc_cost(round):
             if (is_possible(new_place)):
                 cost_grid[new_place[0], new_place[1]] = cost_grid[start[0], start[1]]+1
                 next_round += [new_place]
+                update_expansion_grid(new_place)
     return next_round
 
 cost_grid = np.array(grid)
 cost_grid[cost_grid > 0] = 0
+
+expansion_grid = np.array(grid)
+expansion_grid[expansion_grid > -1] = -1
+
+expansion_counter = 0
+def update_expansion_grid(place):
+    global expansion_counter
+    expansion_grid[place[0], place[1]] = expansion_counter
+    expansion_counter += 1
+
 def search(grid,init,goal,cost):
     # ----------------------------------------
     # insert code here
     # ----------------------------------------
-    next_round = [[0,0]]
+    start = [0,0]
+    next_round = [start]
+    update_expansion_grid(start)
+
     while (len(next_round) > 0):
         next_round = calc_cost(next_round)
 
     #print(cost_grid)
+    print(expansion_grid)
+
     final_cost = cost_grid[goal[0], goal[1]]
     if final_cost == 0:
         return 'fail'
